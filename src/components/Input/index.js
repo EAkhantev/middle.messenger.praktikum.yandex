d60c115e4e20048328a0686acc78c2bd3a929e35
e.akhantev <e.akhantev@s7.ru>
2024-06-20 18:45:45 +0400

    feat: add `link` and `button` components

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,14 +1,3 @@
 import './input.scss';
 import Input from './input.hbs?raw';
 export default Input;
-
-// const label = document.querySelector('label');
-// const input = document.querySelector('input');
-
-// console.log(label);
-// console.log(input);
-
-// input.addEventListener("focusin", () => label.classList.add('isActive'));
-// input.addEventListener("focusout", () => {
-//   if (input.value === '') label.classList.remove('isActive');
-// });
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import Handlebars from 'handlebars';
 import LoginForm from './components/LoginForm'
 import Input from './components/Input'
 import Title from './components/Title'
+import Button from './components/Button'
+import Link from './components/Link'
 
 // const rootElement = document.getElementById('root');
 
 // Handlebars.registerPartial('LoginForm', LoginForm);
-Handlebars.registerPartial('Input', Input);
 Handlebars.registerPartial('Title', Title);
+Handlebars.registerPartial('Input', Input);
+Handlebars.registerPartial('Button', Button);
+Handlebars.registerPartial('Link', Link);
 
 
 const LoginFormFunc = Handlebars.compile(LoginForm);
