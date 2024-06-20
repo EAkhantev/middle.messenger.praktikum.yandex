06a86a02df2c4a2a77c2b5ae5ae06088d03d5767
e.akhantev <e.akhantev@s7.ru>
2024-06-20 16:33:05 +0400

    feat: update styles

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import Handlebars from 'handlebars';
 import LoginForm from './components/LoginForm'
 import Input from './components/Input'
+import Title from './components/Title'
 
-const rootElement = document.getElementById('root');
+// const rootElement = document.getElementById('root');
 
 // Handlebars.registerPartial('LoginForm', LoginForm);
 Handlebars.registerPartial('Input', Input);
+Handlebars.registerPartial('Title', Title);
 
 
 const LoginFormFunc = Handlebars.compile(LoginForm);
 const LoginFormElem = LoginFormFunc();
+document.body.innerHTML = LoginFormElem;
 
 
-console.log(LoginFormElem);
-rootElement.innerHTML = LoginFormElem;
 
+// Handle input focus
+const label = document.querySelector('label');
+const input = document.querySelector('input');
 
+input.addEventListener("focusin", () => {
+  label.classList.add('isActive')
+});
+
+input.addEventListener("focusout", () => {
+  if (input.value === '') label.classList.remove('isActive');
+});
