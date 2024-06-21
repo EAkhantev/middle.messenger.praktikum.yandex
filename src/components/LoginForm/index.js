ab80adf6eb0611f11d798f2a3f5492d4085275e8
e.akhantev <e.akhantev@s7.ru>
2024-06-21 15:45:28 +0400

    feat: add `form` and `signupFrom` components

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,4 +1,11 @@
+import Handlebars from 'handlebars';
 import './loginForm.scss';
 import LoginForm from './loginForm.hbs?raw';
-
 export default LoginForm;
+
+Handlebars.registerHelper('LoginFields', () => {
+  return [
+    {name:"login", labelValue:"Логин", type:"text"},
+    {name:"password_confirm", labelValue:"Пароль (ещё раз)", type:"password"},
+  ]
+});
--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import Handlebars from 'handlebars';
 import LoginForm from './components/LoginForm'
+import SignUpForm from './components/SignUpForm'
+// import FormTest from './components/FormTest'
 import Input from './components/Input'
 import Title from './components/Title'
 import Button from './components/Button'
 import Link from './components/Link'
+import Form from './components/Form'
 
 // const rootElement = document.getElementById('root');
 
-// Handlebars.registerPartial('LoginForm', LoginForm);
+// const FormTestFunc = Handlebars.compile(FormTest);
+// const FormTestElem = FormTestFunc();
+// document.body.innerHTML = FormTestElem;
+
 Handlebars.registerPartial('Title', Title);
 Handlebars.registerPartial('Input', Input);
 Handlebars.registerPartial('Button', Button);
 Handlebars.registerPartial('Link', Link);
-
+Handlebars.registerPartial('Form', Form);
 
 const LoginFormFunc = Handlebars.compile(LoginForm);
-const LoginFormElem = LoginFormFunc();
+const SignUpFormFunc = Handlebars.compile(SignUpForm);
+const LoginFormElem = LoginFormFunc({
+  titleContent: "Вход",
+  buttonContent: "Авторизоваться",
+  linkContent: "Нет аккаунта?",
+  className: "loginForm",
+});
+const SignUpFormElem = SignUpFormFunc({
+  titleContent: "Регистрация",
+  buttonContent: "Зарегистрироваться",
+  linkContent: "Войти",
+  className: "signupForm",
+});
 document.body.innerHTML = LoginFormElem;
+// document.body.innerHTML = SignUpFormElem;
+document.body.innerHTML += SignUpFormElem;
 
 
 
