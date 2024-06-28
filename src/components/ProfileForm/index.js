4b9e65bf2f887eb51956ff17600288096804d9ab
e.akhantev <e.akhantev@s7.ru>
2024-06-28 23:08:32 +0400

    fix: change input name

--- src/components/ProfileForm/index.js
+++ src/components/ProfileForm/index.js
@@ -9,7 +9,7 @@ Handlebars.registerHelper('ProfileInputs', () => {
     {fieldName:"login", labelValue:"Логин", fieldType:"text", defaultValue:"ivanivanov", isDisable:true},
     {fieldName:"first_name", labelValue:"Имя", fieldType:"text", defaultValue:"Илон", isDisable:true},
     {fieldName:"second_name", labelValue:"Фамилия", fieldType:"text", defaultValue:"Маск", isDisable:true},
-    {fieldName:"user_name", labelValue:"Имя в чате", fieldType:"text", defaultValue:"pussyTamer71", isDisable:true},
+    {fieldName:"display_name", labelValue:"Имя в чате", fieldType:"text", defaultValue:"pussyTamer71", isDisable:true},
     {fieldName:"phone", labelValue:"Телефон", fieldType:"tel", defaultValue:"+7 (909) 975 58 13", isDisable:true},
   ]
 });
