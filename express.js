1e246f83ff71cc4962aa73a6fe744e6f2284ab5e
e.akhantev <e.akhantev@s7.ru>
2024-06-28 16:29:59 +0400

    fix: resolve static distribution for run local build

--- express.js
+++ express.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import path from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.static('dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
 
