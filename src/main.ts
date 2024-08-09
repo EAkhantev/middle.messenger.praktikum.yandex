49af873606276b5945ea7f3cc9f2e5b4c2388d41
e.akhantev <e.akhantev@s7.ru>
2024-08-09 23:54:42 +0400

    feat: add Signup page to main

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,33 @@
 // @ts-nocheck
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const rootElement = document.getElementById('root');
 const loginPage = new Login();
+const signupPage = new Signup();
 
 const setPage = (path) => {
   switch (path) {
     case '/login':
-      rootElement.innerHTML = loginPage.render();
+      rootElement.replaceChildren(loginPage.getContent());
       break;
     case '/signup':
-      rootElement.innerHTML = 'signup';
+      rootElement.replaceChildren(signupPage.getContent());
       break;
     case '/chat':
-      rootElement.innerHTML = 'chat';
+      rootElement.replaceChildren(document.createTextNode('chat'));
       break;
     case '/profile':
-      rootElement.innerHTML = 'profile';
+      rootElement.replaceChildren(document.createTextNode('profile'));
       break;
     case '/page404':
-      rootElement.innerHTML = 'page404';
+      rootElement.replaceChildren(document.createTextNode('page404'));
       break;
     case '/page500':
-      rootElement.innerHTML = 'page500';
+      rootElement.replaceChildren(document.createTextNode('page500'));
       break;
     default:
-      rootElement.innerHTML = '';
+      rootElement.replaceChildren(document.createTextNode(''));
       break;
   }
 }
@@ -70,3 +72,11 @@ document.addEventListener('focusout', (event) => {
     if (target.value === '') label.classList.remove('isActive');
   }
 });
+
+setTimeout(() => {
+  loginPage.props.loginForm.props.button.setProps({
+    buttonContent: 'Войти',
+  });
+  loginPage._element.innerHTML = loginPage.render();
+
+}, 3000); 
