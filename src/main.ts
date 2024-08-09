add26a1e37d9862621c75513267107a52cdf2924
e.akhantev <e.akhantev@s7.ru>
2024-08-09 16:30:10 +0400

    feat: add Login page component to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,28 @@
 // @ts-nocheck
-import Handlebars from 'handlebars';
-
-import ChatPage from './components/ChatPage';
-import ProfilePage from './components/ProfilePage';
-import LoginForm from './components/LoginForm';
-import SignUpForm from './components/SignUpForm';
-
-import Title from './components/Title';
-import Input from './components/Input';
-import Button from './components/Button';
-import Link from './components/Link';
-import Form from './components/Form';
-import ChatNav from './components/ChatNav';
-import ChatItem from './components/ChatItem';
-import BtnHideSide from './components/BtnHideSide';
-import ProfileAvatar from './components/ProfileAvatar';
-import ProfileInput from './components/ProfileInput';
-import ProfileAction from './components/ProfileAction';
-import ProfileForm from './components/ProfileForm';
-import ErrorPage from './components/ErrorPage';
+import Login from "./pages/Login";
 
 const rootElement = document.getElementById('root');
-
-Handlebars.registerPartial('Title', Title);
-Handlebars.registerPartial('Input', Input);
-Handlebars.registerPartial('Button', Button);
-Handlebars.registerPartial('Link', Link);
-Handlebars.registerPartial('Form', Form);
-Handlebars.registerPartial('ChatNav', ChatNav);
-Handlebars.registerPartial('ChatItem', ChatItem);
-Handlebars.registerPartial('BtnHideSide', BtnHideSide);
-Handlebars.registerPartial('ProfileAvatar', ProfileAvatar);
-Handlebars.registerPartial('ProfileInput', ProfileInput);
-Handlebars.registerPartial('ProfileAction', ProfileAction);
-Handlebars.registerPartial('ProfileForm', ProfileForm);
-
-const ChatPageFunc = Handlebars.compile(ChatPage);
-const ProfilePageFunc = Handlebars.compile(ProfilePage);
-const LoginFormFunc = Handlebars.compile(LoginForm);
-const SignUpFormFunc = Handlebars.compile(SignUpForm);
-const ErrorPageFunc = Handlebars.compile(ErrorPage);
-
-const ChatPageElem = ChatPageFunc({});
-const ProfilePageElem = ProfilePageFunc({});
-const LoginFormElem = LoginFormFunc({
-  titleContent: "Вход",
-  buttonContent: "Войти",
-  linkContent: "Нет аккаунта?",
-  className: "loginForm",
-});
-const SignUpFormElem = SignUpFormFunc({
-  titleContent: "Регистрация",
-  buttonContent: "Зарегистрироваться",
-  linkContent: "Войти",
-  className: "signupForm",
-});
-const Page404 = ErrorPageFunc({
-  errorTitle: "404",
-  errorDescription: "Не туда попали",
-  linkContent: "Назад к чатам",
-  href: '/chat',
-});
-const Page500 = ErrorPageFunc({
-  errorTitle: "500",
-  errorDescription: "Мы уже фиксим",
-  linkContent: "Назад к чатам",
-  href: '/chat',
-});
-
-
-
-// Handle input focus
-document.addEventListener('focusin', (event) => {
-  const target = event.target;
-
-  if (target.classList.contains('input-field')) {
-    const label = target.labels[0];
-    label.classList.add('isActive');
-  }
-});
-
-document.addEventListener('focusout', (event) => {
-  const target = event.target;
-
-  if (target.classList.contains('input-field')) {
-    const label = target.labels[0];
-    if (target.value === '') label.classList.remove('isActive');
-  }
-});
+const loginPage = new Login();
 
 const setPage = (path) => {
-
   switch (path) {
     case '/login':
-      rootElement.innerHTML = LoginFormElem;
+      rootElement.innerHTML = loginPage.render();
       break;
     case '/signup':
-      rootElement.innerHTML = SignUpFormElem;
+      rootElement.innerHTML = 'signup';
       break;
     case '/chat':
-      rootElement.innerHTML = ChatPageElem;
+      rootElement.innerHTML = 'chat';
       break;
     case '/profile':
-      rootElement.innerHTML = ProfilePageElem;
+      rootElement.innerHTML = 'profile';
       break;
     case '/page404':
-      rootElement.innerHTML = Page404;
+      rootElement.innerHTML = 'page404';
       break;
     case '/page500':
-      rootElement.innerHTML = Page500;
+      rootElement.innerHTML = 'page500';
       break;
     default:
       rootElement.innerHTML = '';
@@ -135,4 +49,24 @@ window.addEventListener('click', (event) => {
 window.addEventListener('popstate', () => {
   const path = window.location.pathname;
   setPage(path);
+});
+
+
+// TODO: move this logic to Input component
+document.addEventListener('focusin', (event) => {
+  const target = event.target;
+
+  if (target.classList.contains('input-field')) {
+    const label = target.labels[0];
+    label.classList.add('isActive');
+  }
+});
+
+document.addEventListener('focusout', (event) => {
+  const target = event.target;
+
+  if (target.classList.contains('input-field')) {
+    const label = target.labels[0];
+    if (target.value === '') label.classList.remove('isActive');
+  }
 });
--- src/utils/render/index.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-export default function render(parentElm, block) {
-  parentElm.appendChild(block.getContent());
-  block.dispatchComponentDidMount();
-
-  return parentElm;
-} 
