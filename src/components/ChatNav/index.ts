ca2baae6d4061c36467f18b3d6fa1a7aacdb0f22
e.akhantev <e.akhantev@s7.ru>
2024-08-10 15:41:52 +0400

    feat: update ChatNav component

--- src/components/ChatNav/index.ts
+++ src/components/ChatNav/index.ts
@@ -1,3 +1,22 @@
+// @ts-nocheck
 import './chatNav.scss';
-import ChatNav from './chatNav.hbs?raw';
-export default ChatNav;
+import Block from "../../utils/Block";
+import Handlebars from 'handlebars';
+import ChatNavTemplate from './chatNav.hbs?raw';
+
+export default class ChatNav extends Block {
+  constructor(props) {
+    super("div", {...props});
+  }
+
+  render() {
+    const template = Handlebars.compile(ChatNavTemplate);
+    return template(this.props);
+  }
+  
+  update() {
+    this._createResources();
+    this._render();
+    // this._element.innerHTML = this.render();
+  }
+}
