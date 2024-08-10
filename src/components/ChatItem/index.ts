89f832f54b43274c638e201941fb2572b7d0f5a3
e.akhantev <e.akhantev@s7.ru>
2024-08-10 15:43:16 +0400

    feat: update ChatItem component

--- src/components/ChatItem/index.ts
+++ src/components/ChatItem/index.ts
@@ -1,3 +1,22 @@
+// @ts-nocheck
 import './chatItem.scss';
-import ChatItem from './chatItem.hbs?raw';
-export default ChatItem;
+import Block from "../../utils/Block";
+import Handlebars from 'handlebars';
+import ChatItemTemplate from './chatItem.hbs?raw';
+
+export default class ChatItem extends Block {
+  constructor(props) {
+    super("div", {...props});
+  }
+
+  render() {
+    const template = Handlebars.compile(ChatItemTemplate);
+    return template(this.props);
+  }
+  
+  update() {
+    this._createResources();
+    this._render();
+    // this._element.innerHTML = this.render();
+  }
+}
