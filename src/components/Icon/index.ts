bd82026726a5dbd043922cb8d3fa23c41d69e237
e.akhantev <e.akhantev@s7.ru>
2024-08-13 14:58:25 +0400

    feat: replace BtnHideSide to Icon component

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import './icon.scss';
+import Block from "../../utils/Block";
+import Handlebars from 'handlebars';
+import IconTemplate from './icon.hbs?raw';
+
+export default class Icon extends Block {
+  constructor(props) {
+    super("div", props);
+  }
+
+  render() {
+    const template = Handlebars.compile(IconTemplate);
+    return template(this.props);
+  }
+  
+  update() {
+    this._element.innerHTML = this.render();
+  }
+}
